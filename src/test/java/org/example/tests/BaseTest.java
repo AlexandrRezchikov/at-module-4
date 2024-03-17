5998805b0537bda9230d92f0f31ee4ee99f3421b
Aleksandr_Rezchikov <rez4ikov@gmail.com>
2024-03-17 14:10:15 +0300

    commit version 1.0

---
+++ src/test/java/org/example/tests/BaseTest.java
@@ -0,0 +1,69 @@
+package org.example.tests;
+
+import org.example.pages.ApplicationForTourPage;
+import org.example.pages.BasePage;
+import org.example.pages.FiltersForSearchPage;
+import org.example.pages.InfoOfTourPage;
+import org.example.pages.MainPage;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import static org.example.common.Config.CLEAR_COOKIES_AND_STORAGE;
+import static org.example.common.Config.HOLD_BROWSER_OPEN;
+import static org.example.constants.Constant.TimeoutVariable.IMPLICIT_WAIT;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    protected BasePage basePage;
+    protected MainPage mainPage;
+    protected FiltersForSearchPage filtersForSearchPage;
+    protected InfoOfTourPage infoOfTour;
+    protected ApplicationForTourPage applicationForTourPage;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+
+        URL remoteUrl = new URL("http://192.168.31.168:4444");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setPlatform(Platform.ANY);
+        desiredCapabilities.setBrowserName("chrome");
+        ChromeOptions options = new ChromeOptions();
+        options.merge(desiredCapabilities).addArguments("--incognito");
+        driver = new RemoteWebDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
+        driver.manage().window().maximize();
+
+        basePage = new BasePage(driver);
+        mainPage = new MainPage(driver);
+        filtersForSearchPage = new FiltersForSearchPage(driver);
+        infoOfTour = new InfoOfTourPage(driver);
+        applicationForTourPage = new ApplicationForTourPage(driver);
+    }
+
+    @AfterMethod
+    public void clearCookiesAndLocalStorage() {
+        if (CLEAR_COOKIES_AND_STORAGE) {
+            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+            driver.manage().deleteAllCookies();
+            javascriptExecutor.executeScript("window.sessionStorage.clear()");
+        }
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void close() {
+        if (HOLD_BROWSER_OPEN) {
+            driver.quit();
+        }
+    }
+}
